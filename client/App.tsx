28bce25629ce225b0ab25c943de0a2890c209883
Rob Squires <rsquires@sans.org>
2021-05-03 15:18:54 +0100

    chore: client structure

--- client/App.tsx
+++ client/App.tsx
@@ -1,23 +1,14 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+import Router from './pages';
+import Reset from './styles/Reset.styles';
 
 const App: React.FC = () => {
-  const [message, setMessage] = useState('');
-
-  const getHello = async () => {
-    try {
-      const req = await fetch('/api/hello');
-      const res = await req.json();
-      setMessage(res.message);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  useEffect(() => {
-    getHello();
-  }, []);
-
-  return <div>{message}</div>;
+  return (
+    <>
+      <Reset />
+      <Router />
+    </>
+  );
 };
 
 export default App;
