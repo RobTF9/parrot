8a60a59eb1b7c805b129c52c19adcef983543d64
Rob Squires <rsquires@sans.org>
2021-05-04 09:06:27 +0100

    feat: database connection

--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,19 @@
 import express from 'express';
+import { json, urlencoded } from 'body-parser';
+import morgan from 'morgan';
 import path from 'path';
+import mongoose from 'mongoose';
+import env from 'dotenv';
+
+env.config();
 
 const app = express();
 
-app.get('/api/hello', function (_, res) {
+app.use(json());
+app.use(urlencoded({ extended: true }));
+app.use(morgan('dev'));
+
+app.get('/api/hello', (_, res) => {
   res.json({ message: 'hello' });
 });
 
@@ -12,4 +22,21 @@ const clientPath = path.join(__dirname, '..', 'client');
 app.use(express.static(clientPath));
 app.use('*', express.static(clientPath));
 
-app.listen(3000);
+// Connect database
+if (typeof process.env.MONGO_URI === 'string') {
+  console.log('Connecting to database...');
+  mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  });
+
+  mongoose.connection.on('connected', () =>
+    console.log('Connected to database')
+  );
+}
+
+// Start server
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server running on ${port}`));
