b137789ea554048c3379cf31b9aaffd242f971e3
Rob Squires <rsquires@sans.org>
2021-05-07 11:33:11 +0100

    fix: session store

--- server/utils/session.ts
+++ server/utils/session.ts
@@ -1,4 +1,5 @@
 import session from 'express-session';
+import ConnectSession from 'connect-mongodb-session';
 import config from '../config';
 
 declare module 'express-session' {
@@ -8,11 +9,23 @@ declare module 'express-session' {
   }
 }
 
+const MongoDBStore = ConnectSession(session);
+
+const store = new MongoDBStore({
+  uri: config.databaseUrl || '',
+  collection: 'sessions',
+});
+
+store.on('error', function (error) {
+  console.log(error);
+});
+
 const authSession = session({
   secret: config.sessionSecret,
   resave: false,
   saveUninitialized: true,
   cookie: { secure: config.isProd },
+  store,
 });
 
 export default authSession;
