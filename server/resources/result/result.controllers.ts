8dd293426fa03968e5c380e478edec3b4be6d99a
Rob Squires <rsquires@sans.org>
2021-06-24 13:48:30 +0100

    feat: endpoints for resume and get

--- server/resources/result/result.controllers.ts
+++ server/resources/result/result.controllers.ts
@@ -29,7 +29,7 @@ export const updateResult: RequestHandler = async (req, res, next) => {
   }
 };
 
-export const getResult: RequestHandler = async (req, res, next) => {
+export const newResult: RequestHandler = async (req, res, next) => {
   try {
     const game = await Game.findById(req.params.id).lean().exec();
 
@@ -39,22 +39,7 @@ export const getResult: RequestHandler = async (req, res, next) => {
         .json({ message: ERROR_MESSAGE.RESOURCE_NOT_FOUND });
     }
 
-    const result = await Result.findOne({
-      game: game._id,
-      finished: false,
-      createdBy: req.session.user,
-    })
-      .populate({ path: 'game' })
-      .populate({ path: 'items', populate: { path: 'item' } })
-      .exec();
-
-    if (result) {
-      return res
-        .status(200)
-        .json({ message: SUCCESS_MESSAGE.GAME_RELOADED, data: result });
-    }
-
-    let newResult = await Result.create({
+    let result = await Result.create({
       game: game._id,
       lexicon: req.session.lexicon?._id,
       createdBy: req.session.user,
@@ -71,18 +56,37 @@ export const getResult: RequestHandler = async (req, res, next) => {
       })),
     });
 
-    newResult = await newResult
+    result = await result
       .populate({ path: 'game' })
       .populate({ path: 'items', populate: { path: 'item' } })
       .execPopulate();
 
     await Game.findByIdAndUpdate(req.params.id, {
-      $push: { results: newResult._id },
+      $push: { results: result._id },
     });
 
     return res
       .status(201)
-      .json({ message: SUCCESS_MESSAGE.GAME_STARTED, data: newResult });
+      .json({ message: SUCCESS_MESSAGE.GAME_STARTED, data: result });
+  } catch (error) {
+    return next(new Error(error));
+  }
+};
+
+export const getResult: RequestHandler = async (req, res, next) => {
+  try {
+    const result = await Result.findById(req.params.id)
+      .populate({ path: 'game' })
+      .populate({ path: 'items', populate: { path: 'item' } })
+      .exec();
+
+    if (!result) {
+      return res
+        .status(404)
+        .json({ message: ERROR_MESSAGE.RESOURCE_NOT_FOUND });
+    }
+
+    return res.status(200).json({ data: result });
   } catch (error) {
     return next(new Error(error));
   }
--- server/resources/result/result.router.ts
+++ server/resources/result/result.router.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import { lexiconActive } from '../../services/auth/auth.middleware';
-import { updateResult, getResult } from './result.controllers';
+import { updateResult, getResult, newResult } from './result.controllers';
 
 const router = Router();
 
 router.use(lexiconActive);
-router.route('/:id').get(getResult).put(updateResult);
+router.route('/new/:id').get(newResult);
+router.route('/:id').put(updateResult).get(getResult);
 
 export default router;
