bb4244e3c909ebf937bea23201ee47f8af808c11
Rob Squires <rsquires@sans.org>
2021-07-10 12:09:19 +0100

    feat: add glow to parrot animation

--- app/styles/Animations.styles.ts
+++ app/styles/Animations.styles.ts
@@ -3,12 +3,12 @@ import { keyframes, Keyframes } from 'styled-components';
 export const LanguagePulse = (l: string): Keyframes => keyframes`
 	0% {
 		transform: scale(0.95);
-		box-shadow: 0 0 0 0 var(--${l}-bg-50);
+		box-shadow: 0 0 0 0 var(--${l}-bg-50), 0 0 0 0 var(--${l}-bg);
 	}
 
 	70% {
 		transform: scale(1);
-		box-shadow: 0 0 0 var(--medium) var(--${l}-bg-25);
+		box-shadow: 0 0 0 var(--medium) var(--${l}-bg-25), 0 0 var(--larger) 0 var(--${l}-bg);
 	}
 
 	100% {
