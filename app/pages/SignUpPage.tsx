c57ad50ff1c9bcbbe3535d3e5f3c22308c3d8f73
Rob Squires <rsquires@sans.org>
2021-07-11 12:06:34 +0100

    feat: wire up sign up

--- app/pages/SignUpPage.tsx
+++ app/pages/SignUpPage.tsx
@@ -1,9 +1,13 @@
 import React, { useState } from 'react';
+import { useAuthContext } from '../context/Auth';
 import Button from '../components/Button';
 import Input from '../components/Input';
 import { Bottom, Main, Middle, Top } from '../styles/Layout.styles';
+import { validateSignup } from '../utils/userValidators';
 
 const SignUpPage: React.FC = () => {
+  const { signUp, authLoading } = useAuthContext();
+  const [errors, setErrors] = useState<UserSubmission>({});
   const [details, setDetails] = useState({
     email: '',
     username: '',
@@ -12,10 +16,22 @@ const SignUpPage: React.FC = () => {
 
   const onChange: React.ChangeEventHandler<HTMLInputElement> = (event) => {
     setDetails({ ...details, [event.target.name]: event.target.value });
+    setErrors({});
+  };
+
+  const onSubmit = (event: React.ChangeEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const validationErrors = validateSignup(details);
+
+    if (Object.keys(validationErrors).length > 0) {
+      setErrors(validationErrors);
+    } else {
+      signUp(details);
+    }
   };
 
   return (
-    <Main>
+    <Main as="form" onSubmit={onSubmit}>
       <Top>
         <h1 className="bold xlarge">Create a Parrot</h1>
         <p className="margin-t">
@@ -28,6 +44,7 @@ const SignUpPage: React.FC = () => {
             label: 'Email',
             value: details.email,
             name: 'email',
+            error: errors.email,
             onChange,
           }}
         />
@@ -36,6 +53,7 @@ const SignUpPage: React.FC = () => {
             label: 'Username',
             value: details.username,
             name: 'username',
+            error: errors.username,
             onChange,
           }}
         />
@@ -46,11 +64,14 @@ const SignUpPage: React.FC = () => {
             name: 'password',
             onChange,
             type: 'password',
+            error: errors.password,
           }}
         />
       </Middle>
       <Bottom>
-        <Button>Sign up</Button>
+        <Button loading={authLoading} type="submit">
+          Sign up
+        </Button>
       </Bottom>
     </Main>
   );
