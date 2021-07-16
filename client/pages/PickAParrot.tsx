8dca40f7e407dc39a84322109f59b9cebedc3eb8
Rob Squires <rsquires@sans.org>
2021-07-16 07:43:52 +0100

    feat: add loading component where needed

--- client/pages/PickAParrot.tsx
+++ client/pages/PickAParrot.tsx
@@ -4,30 +4,34 @@ import { useLexiconContext } from '../context/Lexicon';
 import { getLexicons } from '../data/lexiconResource';
 import { TouchableOpacity } from '../styles/Interactive.styles';
 import { Main, Middle, Top } from '../styles/Layout.styles';
+import Loading from '../components/Loading';
 
 const PickAParrot: React.FC = () => {
   const [lexicons, lexiconsLoading] = getLexicons();
   const { activateLexicon } = useLexiconContext();
 
   return (
-    <Main>
-      <Top>
-        <h1 className="xlarge bold">Pick which parrot to teach today</h1>
-      </Top>
-      <Middle columns="1fr 1fr">
-        {lexicons &&
-          lexicons.data.map(({ _id, language }) => (
-            <TouchableOpacity
-              type="button"
-              key={_id}
-              onClick={() => activateLexicon(_id)}
-            >
-              <Parrot {...{ language: language.name }} />
-              <p className="medium">{language.name}</p>
-            </TouchableOpacity>
-          ))}
-      </Middle>
-    </Main>
+    <>
+      <Loading condition={lexiconsLoading} />
+      <Main>
+        <Top>
+          <h1 className="xlarge bold">Pick which parrot to teach today</h1>
+        </Top>
+        <Middle columns="1fr 1fr">
+          {lexicons &&
+            lexicons.data.map(({ _id, language }) => (
+              <TouchableOpacity
+                type="button"
+                key={_id}
+                onClick={() => activateLexicon(_id)}
+              >
+                <Parrot {...{ language: language.name }} />
+                <p className="medium">{language.name}</p>
+              </TouchableOpacity>
+            ))}
+        </Middle>
+      </Main>
+    </>
   );
 };
 
--- client/router/index.tsx
+++ client/router/index.tsx
@@ -2,9 +2,12 @@ import React from 'react';
 import { useAuthContext } from '../context/Auth';
 import NotAuthenticated from './NotAuthenticated';
 import Authenticated from './Authenticated';
+import Loading from '../components/Loading';
 
 const Router: React.FC = () => {
   const { authenticated } = useAuthContext();
+
+  if (authenticated === undefined) return <Loading condition />;
 
   return authenticated ? <Authenticated /> : <NotAuthenticated />;
 };
