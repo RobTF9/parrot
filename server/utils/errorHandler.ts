74f833ace8a998d51deb09b6bf3b1e3d57281cb5
Rob Squires <rsquires@sans.org>
2021-09-10 10:15:03 +0100

    chore: add logging back in

--- server/utils/errorHandler.ts
+++ server/utils/errorHandler.ts
@@ -2,6 +2,11 @@ import { ErrorRequestHandler } from 'express';
 import { ERROR_MESSAGE } from './constants';
 
 const errorHandler: ErrorRequestHandler = (err, req, res, _) => {
+  console.log('ERROR: ');
+  console.table(err);
+  console.log('REQUEST: ', req.body, req.session);
+  console.log('NEXT: ', _);
+
   if (err) {
     if (err.code && err.code === 11000) {
       return res.status(400).json({ message: ERROR_MESSAGE.DUPLICATE_GENERIC });
