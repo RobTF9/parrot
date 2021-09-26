f146613298a0139263d4b7a41a2a5fb50da9cf1b
Rob Squires <rsquires@sans.org>
2021-09-26 19:29:15 +0100

    chore: run cypress on dev and build

--- cypress/integration/first_spec.ts
+++ cypress/integration/first_spec.ts
@@ -1,14 +1,21 @@
-describe('Log in', () => {
+describe('Log in and activate parrot', () => {
   it('User can visit login page', () => {
     cy.visit('/');
     cy.get('a').contains('Login').click();
     cy.get('h1').should('contain', 'Login to Parrot');
   });
 
+  // check front end validation errors
+
+  // check server errors
+
+  // let user log in
   it('Should let users enter details', () => {
     cy.get('input[name=email]').type('robert.b.s@live.co.uk');
     cy.get('input[name=password]').type('password{enter}');
     cy.get('button').contains('Bengali').click();
     cy.get('h1').should('contain', 'Hey Rob');
   });
+
+  // check parrot can be selected
 });
