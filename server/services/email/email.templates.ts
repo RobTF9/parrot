8c37b4a340e13f6f73fa223e5409d5ccfee467b2
Rob <robtf9@icloud.com>
2022-03-20 07:47:18 +0000

    feat: email template

--- server/services/email/email.templates.ts
+++ server/services/email/email.templates.ts
@@ -13,3 +13,10 @@ export const parrotSharedWithYouTemplate = (
   <p>Hi ${name}, vist Parrot to view the new parrot.</p>
   <a href="${link}">Go to Parrot</a>
 `;
+
+export const onBoardingEmail = (name: string, link: string): string => `
+  <h1>Welcome to the Parrot Beta!</h1>
+  <p>Hi ${name}, thanks for signing up to the beta.</p>
+  <p>If you have any questions please send them to <a href="mailto:support@parrot.to">support@parrot.to</a></p>
+  <a href="${link}">Go to Parrot</a>
+`;
