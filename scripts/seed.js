5edb8030d02ec7e067963d12a8628bafa130ed20
Rob <robtf9@icloud.com>
2022-04-19 17:05:01 +0100

    chore: seed script in single file

--- scripts/seed.js
+++ scripts/seed.js
@@ -2,9 +2,393 @@ const { exec } = require('child_process');
 
 const name = process.argv[2];
 
-function postCollection(collection) {
+const fs = require('fs/promises');
+
+const now = new Date();
+const date = now.toISOString();
+const yesterday = new Date(now - 1);
+
+const users = [
+  {
+    _id: {
+      $oid: '6016b9410266200015c5e7f9',
+    },
+    username: 'User',
+    email: 'user@email.com',
+    password: '$2a$08$swkXPjd4Hb3z28oHTJSBR.O.Q2st79CQk2i9DOoyecx6htPWSufcm',
+    role: 'overlord',
+    createdAt: {
+      $date: '${date}',
+    },
+    updatedAt: {
+      $date: '${date}',
+    },
+    __v: 0,
+  },
+  {
+    _id: {
+      $oid: '6016b9410266200015c5e7f6',
+    },
+    username: 'CypressMailtrapUser',
+    email: 'user@h5wjvmse.mailosaur.net',
+    password: '$2a$08$swkXPjd4Hb3z28oHTJSBR.O.Q2st79CQk2i9DOoyecx6htPWSufcm',
+    createdAt: {
+      $date: '${date}',
+    },
+    updatedAt: {
+      $date: '${date}',
+    },
+    __v: 0,
+  },
+];
+
+const phrase = [
+  {
+    _id: {
+      $oid: '60185d89a7fba10015c88c15',
+    },
+    parrot: {
+      $oid: '6093e4e3103adf787edeb009',
+    },
+    lang: 'কালো',
+    pron: 'Kalo',
+    tran: 'Black',
+    createdBy: {
+      $oid: '6016b9410266200015c5e7f9',
+    },
+    createdAt: {
+      $date: '${date}',
+    },
+    updatedAt: {
+      $date: '${date}',
+    },
+    __v: 0,
+  },
+  {
+    _id: {
+      $oid: '60185d89a7fba10015c88c16',
+    },
+    parrot: {
+      $oid: '6093e4e3103adf787edeb009',
+    },
+    lang: 'হলুদ',
+    pron: 'Holud',
+    tran: 'Yellow',
+    createdBy: {
+      $oid: '6016b9410266200015c5e7f9',
+    },
+    createdAt: {
+      $date: '${date}',
+    },
+    updatedAt: {
+      $date: '${date}',
+    },
+    __v: 0,
+  },
+  {
+    _id: {
+      $oid: '60185d89a7fba10015c88c17',
+    },
+    parrot: {
+      $oid: '6093e4e3103adf787edeb009',
+    },
+    lang: 'লাল',
+    pron: 'Laal',
+    tran: 'Red',
+    createdBy: {
+      $oid: '6016b9410266200015c5e7f9',
+    },
+    createdAt: {
+      $date: '${date}',
+    },
+    updatedAt: {
+      $date: '${date}',
+    },
+    __v: 0,
+  },
+  {
+    _id: {
+      $oid: '60185d89a7fba10015c88c18',
+    },
+    parrot: {
+      $oid: '6093e4e3103adf787edeb010',
+    },
+    lang: 'Ciao',
+    pron: 'cià·o',
+    tran: 'Hello',
+    createdBy: {
+      $oid: '6016b9410266200015c5e7f9',
+    },
+    createdAt: {
+      $date: '${date}',
+    },
+    updatedAt: {
+      $date: '${date}',
+    },
+    __v: 0,
+  },
+];
+
+const game = [
+  {
+    _id: {
+      $oid: '625be07ba1113561379082ab',
+    },
+    parrot: {
+      $oid: '6093e4e3103adf787edeb009',
+    },
+    createdBy: {
+      $oid: '6016b9410266200015c5e7f9',
+    },
+    updateBy: {
+      $oid: '6016b9410266200015c5e7f9',
+    },
+    createdAt: {
+      $date: date,
+    },
+    updatedAt: {
+      $date: date,
+    },
+    results: [
+      {
+        correct: [
+          {
+            $oid: '60185d89a7fba10015c88c17',
+          },
+          {
+            $oid: '60185d89a7fba10015c88c16',
+          },
+          {
+            $oid: '60185d89a7fba10015c88c15',
+          },
+        ],
+        played: true,
+        createdAt: date,
+        attempted: [
+          {
+            $oid: '60185d89a7fba10015c88c17',
+          },
+          {
+            $oid: '60185d89a7fba10015c88c16',
+          },
+          {
+            $oid: '60185d89a7fba10015c88c15',
+          },
+        ],
+      },
+      {
+        correct: [
+          {
+            $oid: '60185d89a7fba10015c88c17',
+          },
+        ],
+        played: true,
+        createdAt: yesterday,
+        attempted: [
+          {
+            $oid: '60185d89a7fba10015c88c17',
+          },
+          {
+            $oid: '60185d89a7fba10015c88c16',
+          },
+          {
+            $oid: '60185d89a7fba10015c88c15',
+          },
+        ],
+      },
+    ],
+    phrases: [
+      {
+        $oid: '60185d89a7fba10015c88c17',
+      },
+      {
+        $oid: '60185d89a7fba10015c88c16',
+      },
+      {
+        $oid: '60185d89a7fba10015c88c15',
+      },
+    ],
+  },
+  {
+    _id: {
+      $oid: '625be07ba1113561379082ac',
+    },
+    parrot: {
+      $oid: '6093e4e3103adf787edeb009',
+    },
+    createdBy: {
+      $oid: '6016b9410266200015c5e7f9',
+    },
+    updateBy: {
+      $oid: '6016b9410266200015c5e7f9',
+    },
+    createdAt: {
+      $date: date,
+    },
+    updatedAt: {
+      $date: date,
+    },
+    results: [
+      {
+        correct: [
+          {
+            $oid: '60185d89a7fba10015c88c17',
+          },
+        ],
+        played: true,
+        createdAt: date,
+        attempted: [
+          {
+            $oid: '60185d89a7fba10015c88c17',
+          },
+          {
+            $oid: '60185d89a7fba10015c88c16',
+          },
+          {
+            $oid: '60185d89a7fba10015c88c15',
+          },
+        ],
+      },
+    ],
+    phrases: [
+      {
+        $oid: '60185d89a7fba10015c88c17',
+      },
+      {
+        $oid: '60185d89a7fba10015c88c16',
+      },
+      {
+        $oid: '60185d89a7fba10015c88c15',
+      },
+    ],
+  },
+  {
+    _id: {
+      $oid: '625be07ba1113561379082ad',
+    },
+    parrot: {
+      $oid: '6093e4e3103adf787edeb009',
+    },
+    createdBy: {
+      $oid: '6016b9410266200015c5e7f9',
+    },
+    updateBy: {
+      $oid: '6016b9410266200015c5e7f9',
+    },
+    createdAt: {
+      $date: date,
+    },
+    updatedAt: {
+      $date: date,
+    },
+    results: [],
+    phrases: [
+      {
+        $oid: '60185d89a7fba10015c88c17',
+      },
+      {
+        $oid: '60185d89a7fba10015c88c16',
+      },
+      {
+        $oid: '60185d89a7fba10015c88c15',
+      },
+    ],
+  },
+  {
+    _id: {
+      $oid: '625be07ba1113561379082ae',
+    },
+    parrot: {
+      $oid: '6093e4e3103adf787edeb009',
+    },
+    createdBy: {
+      $oid: '6016b9410266200015c5e7f9',
+    },
+    updateBy: {
+      $oid: '6016b9410266200015c5e7f9',
+    },
+    createdAt: {
+      $date: date,
+    },
+    updatedAt: {
+      $date: date,
+    },
+    results: [],
+    phrases: [
+      {
+        $oid: '60185d89a7fba10015c88c17',
+      },
+      {
+        $oid: '60185d89a7fba10015c88c16',
+      },
+      {
+        $oid: '60185d89a7fba10015c88c15',
+      },
+    ],
+  },
+];
+
+const parrots = [
+  {
+    _id: {
+      $oid: '6093e4e3103adf787edeb009',
+    },
+    createdBy: {
+      $oid: '6016b9410266200015c5e7f9',
+    },
+    createdAt: {
+      $date: '${date}',
+    },
+    updatedAt: {
+      $date: '${date}',
+    },
+    language: {
+      name: 'Bengali',
+      htmlCode: 'bn',
+      langCode: 'bn-BN',
+      isRomanLanguage: false,
+    },
+    goals: {
+      phrase: 3,
+      games: 1,
+    },
+  },
+  {
+    _id: {
+      $oid: '6093e4e3103adf787edeb010',
+    },
+    createdBy: {
+      $oid: '6016b9410266200015c5e7f9',
+    },
+    createdAt: {
+      $date: '${date}',
+    },
+    updatedAt: {
+      $date: '${date}',
+    },
+    language: {
+      name: 'Italian',
+      htmlCode: 'it',
+      langCode: 'it-IT',
+      isRomanLanguage: true,
+    },
+    goals: {
+      phrase: 2,
+      games: 1,
+    },
+  },
+];
+
+async function postCollection(collection, object) {
+  await fs.writeFile(
+    `./scripts/seed-data/${collection}.json`,
+    JSON.stringify(object),
+    (err) => {
+      if (err) throw err;
+    }
+  );
   exec(
-    `mongoimport --uri mongodb://localhost:27017/${name} --file ${__dirname}/data/${collection}.json --jsonArray --drop
+    `mongoimport --uri mongodb://localhost:27017/${name} --file ${__dirname}/seed-data/${collection}.json --jsonArray --drop
     `,
     (error, stdout, stderr) => {
       if (error) {
@@ -20,7 +404,7 @@ function postCollection(collection) {
   );
 }
 
-postCollection('users');
-postCollection('parrots');
-postCollection('phrase');
-postCollection('game');
+postCollection('users', users);
+postCollection('parrots', parrots);
+postCollection('phrase', phrase);
+postCollection('game', game);
